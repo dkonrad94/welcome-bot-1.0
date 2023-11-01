096a5aca0ad581872e9c5992799fe2c4671d73a6
dkonrad94 <149543773+dkonrad94@users.noreply.github.com>
2023-11-01 04:03:11 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,65 @@
+{
+  "Welcome": {
+
+        "Embed": {
+          "enabled": true,
+          "channelId": "1167961389676114041",
+          "message": "Hello {member}, Welcome in to the Server 🎊",
+          "username": "{username}#{discriminator}",
+          "custom_message": "Visual Development - Store",
+          "footer_message": "You are the {count}.th member!",
+          "color": "#29f19c",
+          "textcolor": "WHITE"
+        },
+
+        "Emoji": {
+          "enabled": true, 
+          "first": "👋",
+          "second": "👋"
+        },
+
+        "AddRole": {
+          "enabled": true, 
+          "roleID": "1004377552883830945"
+        }
+  },
+
+
+  "Leave": {
+
+    "Embed": {
+      "enabled": true,
+      "channelId": "1167961389676114041",
+      "color": "#ec213e",
+      "textcolor": "WHITE",
+      "message": "Hello {member}, Welcome in to the Server 🎊",
+      "username": "{username}#{discriminator}",
+      "custom_message": "Visual Development - Store",
+      "footer_message": "Total members: {count}"
+    },
+
+    "Emoji": {
+      "enabled": true, 
+      "leave": "🤷‍♂️"
+    }
+},
+
+
+  "activityConfig": [
+    {
+      "interval": 5000,
+      "type": "WATCHING",
+      "text": "🛒 First activity"
+    },
+    {
+      "interval": 10000,
+      "type": "WATCHING",
+      "text": "🎶 Second activity"
+    },
+    {
+      "interval": 15000,
+      "type": "WATCHING",
+      "text": "🔑 Third activity"
+    }
+  ]
+}
